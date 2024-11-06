f316a9ff21fac2123291172eb27dfb2054a5458a
rlatksk <justinsalim73@gmail.com>
2024-11-06 15:58:50 +0700

    Add database model for places

--- server/models/Place.js
+++ server/models/Place.js
@@ -1,1 +1,29 @@
-// database model schema
+const mongoose = require('mongoose');
+
+const placeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['meal', 'dessert', 'snack'],
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  openingTime: {
+    type: String,
+    required: true,
+  },
+});
+
+const Place = mongoose.model('Place', placeSchema);
+
+module.exports = Place;
