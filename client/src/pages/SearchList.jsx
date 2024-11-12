21cdab33b56bbcd7e26646a0e4fc48e8a8e0996f
Scorkinn <ruben.tobiachaiyadi@yahoo.com>
2024-11-12 22:55:21 +0700

    Added shadcn pagination

--- client/src/pages/SearchList.jsx
+++ client/src/pages/SearchList.jsx
@@ -4,6 +4,15 @@ import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from '@/comp
 import CheHunTiau from '@/assets/images/CheHunTiau.jpg';
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
 
 function SearchList() {
   const items = ['Apple', 'Banana', 'Orange', 'Grapes', 'A', 'Apple', 'Banana', 'Orange', 'Grapes', 'A', 'B'];
@@ -52,7 +61,30 @@ function SearchList() {
           </Card>
           ))}
         </div>
-        
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem >
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">1</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#" isActive>
+                2
+              </PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationLink href="#">3</PaginationLink>
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem >
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
       </div>
     </div>
   )
