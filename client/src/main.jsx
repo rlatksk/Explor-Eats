bc42ac9896d2501bc38d40d1c807c2e06e600140
Willsen Yogi <willsenyp21@gmail.com>
2024-11-13 20:26:14 +0700

    fixed react router configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from 'react'
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import './index.css'
-import App from './App'
-import Home from './pages/home/Home'
+import { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import Home from './pages/home/Home';
 import FoodDetails from './pages/details/FoodDetails';
 import PlacesDetails from './pages/details/PlacesDetails';
 import SearchList from './pages/SearchList';
@@ -16,40 +13,10 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/details/food", // Add id here later
-        element: <FoodDetails />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/details/place", // Add id here later
-        element: <PlacesDetails />,
-      },
-    ],
-  },
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/list", // Add id here later
-        element: <SearchList />,
-      },
+      { path: "/", element: <Home /> },
+      { path: "/details/food", element: <FoodDetails /> },
+      { path: "/details/place", element: <PlacesDetails /> },
+      { path: "/list", element: <SearchList /> }
     ],
   },
 ]);
