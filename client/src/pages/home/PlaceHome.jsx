8bc713b3ced9546f4b4c4288a65d56569923d747
Willsen Yogi <willsenyp21@gmail.com>
2024-11-20 21:44:38 +0700

    minor fix

--- client/src/pages/home/PlaceHome.jsx
+++ client/src/pages/home/PlaceHome.jsx
@@ -60,7 +60,7 @@ function PlaceHome() {
                     {city.culinaryDescription}
                   </p>
                   <Button className="min-w-[34.375rem]" variant="default2">
-                    <Link to={`/details/place/${city.city.toLowerCase()}`}>
+                    <Link to={`/details/place/${city.city}`}>
                       Start Discovering
                     </Link>
                   </Button>
