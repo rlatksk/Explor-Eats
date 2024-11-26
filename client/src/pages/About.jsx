80d9b517e3ba54b566d831d1d4dd1fb4b1f69742
rlatksk <justinsalim73@gmail.com>
2024-11-26 19:46:06 +0700

    Changed about us description and added photo of us

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import PageDesc from '@/layout/PageDesc';
-import CheHunTiau from '@/assets/images/CheHunTiau.jpg';
+import React from "react";
+import PageDesc from "@/layout/PageDesc";
+import Member from "@/assets/images/Member.jpg";
 
 function About() {
   return (
     <div>
-        <PageDesc title={'About Us'} route={null} />
-        <div className='flex flex-col h-fit items-center bg-primaryColor gap-[2rem] pt-[4.375rem] pb-[3.125rem] px-[20rem]'>
-            <img src={CheHunTiau} alt="" className='h-[20rem] w-[45rem] object-cover ' />
-            <p className='text-secondaryColor font-semibold text-[1.2 rem] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum rutrum elit, ut mattis dolor vulputate eu. Nam vel libero ex. Quisque finibus ligula ac tempus dapibus. Cras sed dictum ligula. Vivamus hendrerit egestas laoreet. Pellentesque varius in nibh eu elementum. In congue sed lacus in condimentum. Mauris ut accumsan magna. Aliquam aliquet accumsan metus, sed congue ipsum hendrerit eu. Fusce condimentum laoreet lacus eu dapibus. Proin a tempus justo. Nullam arcu nunc, fermentum molestie diam in, pellentesque ultrices orci. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed mauris libero, pretium eu porttitor vel, pretium vel purus.</p>
-        </div>
+      <PageDesc title={"About Us"} route={null} />
+      <div className="flex flex-col h-fit items-center bg-primaryColor gap-[2rem] pt-[4.375rem] pb-[3.125rem] px-[20rem]">
+        <img
+          src={Member}
+          alt=""
+          className="h-[20rem] w-[45rem] object-contain"
+        />
+        <p className="text-secondaryColor font-semibold text-[1.2 rem] text-center">
+          We are a team of students who share a deep passion for food and
+          exploration. As a food enthusiasts, we believe that every meal tells a
+          story and every dish offers a glimpse into the culture and creativity
+          of its origin. Explor-Eats was born from our desire to connect people
+          with incredible dining experiences, from hidden gems in the city to
+          iconic dishes everyone should try. Our mission is simple: to make it
+          easier for you to find amazing food, discover unique eateries, and
+          share your love for all things culinary. Whether you're a casual diner
+          or a seasoned foodie, Explor-Eats is here to guide you on a flavorful
+          journey you won’t forget.
+        </p>
+      </div>
     </div>
-    
-
-  )
+  );
 }
 
-export default About
+export default About;
