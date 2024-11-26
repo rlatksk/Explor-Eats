bd251fa3371006dbb5b0d2bb6fcdee8f75687103
rlatksk <justinsalim73@gmail.com>
2024-11-26 19:46:26 +0700

    Changed various of description in the home page

--- client/src/pages/home/FoodHome.jsx
+++ client/src/pages/home/FoodHome.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Carousel } from '@/components/Carousel';
-import CheHunTiau from '@/assets/images/CheHunTiau.jpg';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { Carousel } from "@/components/Carousel";
+import CheHunTiau from "@/assets/images/CheHunTiau.jpg";
+import axios from "axios";
 
 function FoodHome() {
   const [slides, setSlides] = useState([]);
@@ -10,7 +10,8 @@ function FoodHome() {
   const OPTIONS = { loop: true };
 
   useEffect(() => {
-    axios.get('/api/food')
+    axios
+      .get("/api/food")
       .then((response) => {
         const foodData = response.data;
 
@@ -25,20 +26,23 @@ function FoodHome() {
         setLoading(false);
       })
       .catch((error) => {
-        setError('Error fetching food data');
+        setError("Error fetching food data");
         setLoading(false);
       });
   }, []);
 
   return (
-    <div className='flex flex-col h-fit items-center bg-primaryColor gap-[3.125rem] pt-[4.375rem] pb-[3.125rem] px-[10rem]'>
-      <div className='flex flex-col gap-[1.125rem] items-center'>
-        <hr className='border-t-8 border-secondaryColor w-full rounded-full' />
-        <h1 className='text-secondaryColor font-bold text-[4rem]'>THE FOODS</h1>
-        <p className='text-secondaryColor font-semibold text-[0.938rem] text-center w-4/5'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec interdum rutrum elit, ut mattis dolor vulputate eu. Nam vel libero ex. Quisque finibus ligula ac tempus dapibus. Cras sed dictum ligula. Vivamus hendrerit egestas laoreet. Pellentesque varius in nibh eu elementum. In congue sed lacus in condimentum.
+    <div className="flex flex-col h-fit items-center bg-primaryColor gap-[3.125rem] pt-[4.375rem] pb-[3.125rem] px-[10rem]">
+      <div className="flex flex-col gap-[1.125rem] items-center">
+        <hr className="border-t-8 border-secondaryColor w-full rounded-full" />
+        <h1 className="text-secondaryColor font-bold text-[4rem]">THE FOODS</h1>
+        <p className="text-secondaryColor font-semibold text-[0.938rem] text-center w-4/5">
+          From savory street snacks to sweet desert, we bring you the best
+          culinary experiences from every corner. Explore a diverse range of
+          foods that highlight tradition, creativity, and bold flavors. Each
+          bite is a journey into the heart of local and international cuisine.
         </p>
-        <hr className='border-t-8 border-secondaryColor w-11/12 rounded-full' />
+        <hr className="border-t-8 border-secondaryColor w-11/12 rounded-full" />
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
@@ -46,9 +50,8 @@ function FoodHome() {
         ) : (
           <Carousel slides={slides} options={OPTIONS} />
         )}
-        
       </div>
-        <hr className='border-t-8 border-secondaryColor w-11/12 rounded-full' />
+      <hr className="border-t-8 border-secondaryColor w-11/12 rounded-full" />
     </div>
   );
 }
