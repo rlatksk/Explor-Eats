6792e4bdb113f73b6a8cd368482ae4b3c62847e5
rlatksk <justinsalim73@gmail.com>
2024-11-27 00:30:40 +0700

    Update axios instance to use import.meta.env for base URL

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config()
 
 const axiosInstance = axios.create({
-  baseURL: process.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 export default axiosInstance;
