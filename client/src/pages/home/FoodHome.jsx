9a7647a1d382a50ddac0f55a2622d4318b08fad7
rlatksk <justinsalim73@gmail.com>
2024-12-01 00:42:07 +0700

    Add background images to FoodHome and PlaceHome components

--- client/src/pages/home/FoodHome.jsx
+++ client/src/pages/home/FoodHome.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Carousel } from "@/components/Carousel";
 import CheHunTiau from "@/assets/images/CheHunTiau.jpg";
-import axiosInstance from '@/lib/axios';
+import borderBrown from "@/assets/images/border-brown.png";
+import axiosInstance from "@/lib/axios";
 
 function FoodHome() {
   const [slides, setSlides] = useState([]);
@@ -32,8 +33,11 @@ function FoodHome() {
   }, []);
 
   return (
-    <div className="flex flex-col h-fit items-center bg-primaryColor gap-[3.125rem] pt-[4.375rem] pb-[3.125rem] px-[10rem]">
-      <div className="flex flex-col gap-[1.125rem] items-center">
+    <div
+      style={{ "--image-url": `url(${borderBrown})` }}
+      className="flex flex-col h-fit items-center bg-primaryColor bg-no-repeat gap-[3.125rem] bg-[image:var(--image-url)] bg-[length:100%] bg-center pt-[4.375rem] pb-[3.125rem] px-[10rem]"
+    >
+      <div className="flex flex-col gap-[1.125rem] items-center overflow-x-hidden">
         <hr className="border-t-8 border-secondaryColor w-full rounded-full" />
         <h1 className="text-secondaryColor font-bold text-[4rem]">THE FOODS</h1>
         <p className="text-secondaryColor font-semibold text-[0.938rem] text-center w-4/5">
